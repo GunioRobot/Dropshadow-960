8e977ba70d238b5a40a0e73f1bc1b9c8adef3afe
bhornseth <bhornseth@gmail.com>
2010-01-10 15:23:23 -0600

    Cleaned up the archives.php code a bit

--- archives.php
+++ archives.php
@@ -3,16 +3,15 @@
     <div id="contentleft">
         <h1>Archives</h1>
         <div id="archives-list">
-        <?php  $allPosts = get_posts('orderby=date&order=DESC'); ?>
-        <?php foreach ($allPosts as $post): ?>
-            <?php setup_postdata ($post); ?>
+        <?php query_posts('orderby=date&order=DESC')?>
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>    
             <?php $thisPostDate = the_date('F Y', '', '', false); ?>
             <?php if (isset($curDate) && $curDate !== $thisPostDate): ?>
                 </ul>
             <?php endif; ?>
-            
-            
-                
+
+
+
             <?php if (!isset($curDate) || $curDate !== $thisPostDate): ?>
                 <div class="archives-section"><?php the_time('F Y'); ?></div>
                 <ul>
@@ -20,19 +19,24 @@
             <?php endif; ?>
             <li>
             <span class="bold"><?php the_time('d'); ?></span> : 
-              <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
             <?php 
                 $cats = array();
                 foreach((get_the_category()) as $category) {
                     $cats[] = strtolower($category->cat_name) ;
                 } 
-                
             ?>
             <span class="lighter"><?php echo implode(', ', $cats); ?></span>
-            
+
             </li>
-        <?php endforeach; ?>
+                
+            
+        <?php endwhile;?>
         </ul>
+        <?php else: ?>
+            <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>    
+        <?php endif; ?>        
+
         </div>
     </div>
     <?php include(TEMPLATEPATH."/r_sidebar.php");?>
